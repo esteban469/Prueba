a60a79d05f16cb25b47a481099b79074a599685d
Esteban Padilla <estebanpadilla469@gmail.com>
2024-08-22 15:24:02 -0600

    Se remplazaron archivos Program y Lecturas

--- obj/Debug/net8.0/Prueba.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a204957f157bfde965952c22b72a345103997e42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
